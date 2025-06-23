7f826e689e1915a257b02e518a6048d653ec7b5c
opapy1409 <sirikamol1409@gmail.com>
2025-06-23 03:13:44 +0000

    add new file

---
+++ cell04/ex04/round_up.py
@@ -0,0 +1,4 @@
+num_input = input("Give me a number: ")
+number = float(num_input)
+rounded = round(number)
+print(f"The rounded number is {rounded}.")
---
+++ cell04/ex05/round_up.py
@@ -0,0 +1,3 @@
+letter = input("Give me an uppercase letter: ")
+lowercase = letter.lower()
+print(f"The lowercase letter is {lowercase}.")
